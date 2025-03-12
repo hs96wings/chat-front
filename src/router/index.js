bfe4d595847df22742f37f9868393d6d92c7b014
hs96wings <hs96wings@gmail.com>
2025-03-12 12:08:46 +0900

    회원가입

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router"
+import MemberCreate from "@/views/MemberCreate.vue"
 
-const routes = []
+const routes = [
+    {
+        path: '/member/create',
+        name: 'MemberCreate',
+        component: MemberCreate
+    }
+
+]
 
 const router = createRouter({
     history: createWebHistory(),
